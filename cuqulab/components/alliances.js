d6ed5237eeda9baf5f85d9dfde59e6b307f46df8
Diana Gabriela Miños <eurekagaudeamus@gmail.com>
2021-06-03 21:37:44 -0300

    Agrego animación a componente Alliance

--- cuqulab/components/alliances.js
+++ cuqulab/components/alliances.js
@@ -5,17 +5,17 @@ const Alliances = () => {
     return (
         <div className={styles.allianceContainer}>
             <div className='row'>
-                <h2 className='text-center'>Alianzas</h2>
+                <h2 className='animate__animated animate__bounce text-center'>Alianzas</h2>
                 <hr />
                 <div className={styles.thumb}>
                     <div className='col-md-6'>
-                        <div >
-                            <Image clasName={styles.imgAlliance} src='/images/LOGO.BRANDON.png' alt='casa brandon' width='300' height='100' />
+                        <div clasName={styles.imgAlliance}>
+                            <Image className='animate__animated animate__fadeInDown animate__delay-2s' src='/images/LOGO.BRANDON.png' alt='casa brandon' width='300' height='100' />
                         </div>
                     </div>
                     <div className='col-md-6 text-center'>
                         <div className={styles.pAlliance}>
-                            <p>Brandon por la Igualdad/Equidad de Derechos y Oportunidades Asociación Civil LGTBIQ+ 🏳️‍🌈 Sede social @casabrandon 💥 20 años de Cultura LGTBIQ+ 😻</p>
+                            <p className='animate__animated animate__fadeInRight animate__delay-2s'>Brandon por la Igualdad/Equidad de Derechos y Oportunidades Asociación Civil LGTBIQ+ 🏳️‍🌈 Sede social @casabrandon 💥 20 años de Cultura LGTBIQ+ 😻</p>
                         </div>
                     </div>
                 </div>
