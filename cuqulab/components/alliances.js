d57d81fc2443fda03c3b42a77098e4ec67abbf95
Diana Gabriela Miños <eurekagaudeamus@gmail.com>
2021-06-05 18:24:00 -0300

    Creo nueva versión de sección Alianzas con logos

--- cuqulab/components/alliances.js
+++ cuqulab/components/alliances.js
@@ -1,39 +1,87 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import { Instagram } from 'react-bootstrap-icons'
-import { Globe } from 'react-bootstrap-icons'
 import styles from '../styles/Alliance.module.css'
 
 const Alliances = () => {
     return (
         <div className={styles.allianceContainer}>
             <div className='row'>
-                <h2 className='animate__animated animate__bounce text-center'>Alianzas</h2>
-                <hr />
-                <div className={styles.thumb}>
-                    <div className='col-md-6'>
-                        <div clasName={styles.imgAlliance}>
-                            <Image className='animate__animated animate__fadeInDown animate__delay-2s' src='/images/LOGO.BRANDON.png' alt='casa brandon' width='300' height='100' />
-                        </div>
+                <h2 className='text-center'>Alianzas</h2>
+            </div>
+            <hr />
+            <div className='row'>
+                <div className='col-md-3 col-sm-6 col-xs-12 text-center'>
+                    <div className={styles.imgAlliance}>
+                        <Link href='https://www.instagram.com/brandonporlaigualdad/'>
+                            <a >
+                                <Image src='/images/logobrandonalianza.png' alt='casa brandon alianza' width='150' height='150' />
+                            </a>
+                        </Link>
                     </div>
-                    <div className='col-md-6 text-center'>
-                        <div className={styles.pAlliance}>
-                            <p className='animate__animated animate__fadeInRight animate__delay-2s'>Brandon por la Igualdad/Equidad de Derechos y Oportunidades Asociación Civil LGTBIQ+ 🏳️‍🌈 Sede social @casabrandon 💥 20 años de Cultura LGTBIQ+ 😻</p>
-                        </div>
-                        <ul className='list-unstyled list-inline'>
-                            <li className='list-inline-item'>
-                                <Link href='https://www.instagram.com/brandonporlaigualdad/'>
-                                    <a className='btn-lg'><Instagram />
-                                    </a>
-                                </Link>
-                            </li>
-                            <li className='list-inline-item'>
-                                <Link href='http://brandon.org.ar/'>
-                                    <a className='btn-lg'><Globe />
-                                    </a>
-                                </Link>
-                            </li>
-                        </ul>
+                </div>
+                <div className='col-md-3 col-sm-6 col-xs-12 text-center'>
+                    <div className={styles.imgAlliance}>
+                        <Link href='https://www.instagram.com/clicvillacrespo/'>
+                            <a>
+                                <Image src='/images/logoclicalianza.png' alt='c.l.i.c' width='150' height='150' />
+                            </a>
+                        </Link>
+                    </div>
+                </div>
+                <div className='col-md-3 col-sm-6 col-xs-12 text-center'>
+                    <div className={styles.imgAlliance}>
+                        <Link href='https://www.instagram.com/movimientodejuventudestrans/'>
+                            <a>
+                                <Image src='/images/logomjtalianza.png' alt='mjt' width='150' height='150' />
+                            </a>
+                        </Link>
+                    </div>
+                </div>
+                <div className='col-md-3 col-sm-6 col-xs-12 text-center'>
+                    <div className={styles.imgAlliance}>
+                        <Link href='https://www.instagram.com/vuelaelpez/'>
+                            <a>
+                                <Image src='/images/logovlpalianza.png' alt='vuela el pez' width='150' height='150' />
+                            </a>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+            <div className='row'>
+                <div className='col-md-3 col-sm-6 col-xs-12 text-center'>
+                    <div className={styles.imgAlliance}>
+                        <Link href='https://www.instagram.com/mochacelis/'>
+                            <a>
+                                <Image src='/images/logomochacelisalianza.png' alt='mocha celis' width='150' height='150' />
+                            </a>
+                        </Link>
+                    </div>
+                </div>
+                <div className='col-md-3 col-sm-6 col-xs-12 text-center'>
+                    <div className={styles.imgAlliance}>
+                        <Link href='https://www.instagram.com/casabrandon/'>
+                            <a>
+                                <Image src='/images/logocasabrandon.png' alt='casa brandon' width='150' height='150' />
+                            </a>
+                        </Link>
+                    </div>
+                </div>
+                <div className='col-md-3 col-sm-6 col-xs-12 text-center'>
+                    <div className={styles.imgAlliance}>
+                        <Link href='https://www.instagram.com/brandonporlaigualdad/'>
+                            <a>
+                                <Image src='/images/logobrandonalianza.png' alt='casa brandon alianza' width='150' height='150' />
+                            </a>
+                        </Link>
+                    </div>
+                </div>
+                <div className='col-md-3 col-sm-6 col-xs-12 text-center'>
+                    <div className={styles.imgAlliance}>
+                        <Link href='https://www.instagram.com/luzuriagaclubsocial/'>
+                            <a>
+                                <Image src='/images/logoluzuriagaalianza.png' alt='luzuriaga club social' width='150' height='150' />
+                            </a>
+                        </Link>
                     </div>
                 </div>
             </div>
