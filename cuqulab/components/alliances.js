42a95fb9d20caa51eef5a0c97673af1f7b58e400
Diana Gabriela Miños <eurekagaudeamus@gmail.com>
2021-06-05 21:35:25 -0300

    Agrego nuevo logo y estilos a sección Alianzas

--- cuqulab/components/alliances.js
+++ cuqulab/components/alliances.js
@@ -14,63 +14,7 @@ const Alliances = () => {
                     <div className={styles.imgAlliance}>
                         <Link href='https://www.instagram.com/brandonporlaigualdad/'>
                             <a >
-                                <Image src='/images/logobrandonalianza.png' alt='casa brandon alianza' width='150' height='150' />
-                            </a>
-                        </Link>
-                    </div>
-                </div>
-                <div className='col-md-3 col-sm-6 col-xs-12 text-center'>
-                    <div className={styles.imgAlliance}>
-                        <Link href='https://www.instagram.com/clicvillacrespo/'>
-                            <a>
-                                <Image src='/images/logoclicalianza.png' alt='c.l.i.c' width='150' height='150' />
-                            </a>
-                        </Link>
-                    </div>
-                </div>
-                <div className='col-md-3 col-sm-6 col-xs-12 text-center'>
-                    <div className={styles.imgAlliance}>
-                        <Link href='https://www.instagram.com/movimientodejuventudestrans/'>
-                            <a>
-                                <Image src='/images/logomjtalianza.png' alt='mjt' width='150' height='150' />
-                            </a>
-                        </Link>
-                    </div>
-                </div>
-                <div className='col-md-3 col-sm-6 col-xs-12 text-center'>
-                    <div className={styles.imgAlliance}>
-                        <Link href='https://www.instagram.com/vuelaelpez/'>
-                            <a>
-                                <Image src='/images/logovlpalianza.png' alt='vuela el pez' width='150' height='150' />
-                            </a>
-                        </Link>
-                    </div>
-                </div>
-            </div>
-            <div className='row'>
-                <div className='col-md-3 col-sm-6 col-xs-12 text-center'>
-                    <div className={styles.imgAlliance}>
-                        <Link href='https://www.instagram.com/mochacelis/'>
-                            <a>
-                                <Image src='/images/logomochacelisalianza.png' alt='mocha celis' width='150' height='150' />
-                            </a>
-                        </Link>
-                    </div>
-                </div>
-                <div className='col-md-3 col-sm-6 col-xs-12 text-center'>
-                    <div className={styles.imgAlliance}>
-                        <Link href='https://www.instagram.com/casabrandon/'>
-                            <a>
-                                <Image src='/images/logocasabrandon.png' alt='casa brandon' width='150' height='150' />
-                            </a>
-                        </Link>
-                    </div>
-                </div>
-                <div className='col-md-3 col-sm-6 col-xs-12 text-center'>
-                    <div className={styles.imgAlliance}>
-                        <Link href='https://www.instagram.com/brandonporlaigualdad/'>
-                            <a>
-                                <Image src='/images/logobrandonalianza.png' alt='casa brandon alianza' width='150' height='150' />
+                                <Image src='/images/logobrandonalianza.png' alt='Casa Brandon alianza' width='200' height='200' />
                             </a>
                         </Link>
                     </div>
@@ -79,7 +23,64 @@ const Alliances = () => {
                     <div className={styles.imgAlliance}>
                         <Link href='https://www.instagram.com/luzuriagaclubsocial/'>
                             <a>
-                                <Image src='/images/logoluzuriagaalianza.png' alt='luzuriaga club social' width='150' height='150' />
+                                <Image src='/images/logoluzuriagaalianza.png' alt='Luzuriaga club social' width='150' height='150' />
+                            </a>
+                        </Link>
+                    </div>
+                </div>
+                <div className='col-md-3 col-sm-6 col-xs-12 text-center'>
+                    <div className={styles.imgAlliance}>
+                        <Link href='https://www.instagram.com/movimientodejuventudestrans/'>
+                            <a>
+                                <Image src='/images/logomjtalianza.png' alt='Movimiento de Juventudes Trans' width='150' height='150' />
+                            </a>
+                        </Link>
+                    </div>
+                </div>
+                <div className='col-md-3 col-sm-6 col-xs-12 text-center'>
+                    <div className={styles.imgAlliance}>
+                        <Link href='https://www.instagram.com/mochacelis/'>
+                            <a>
+                                <Image src='/images/logomochacelisalianza.png' alt='Mocha Celis' width='200' height='200' />
+                            </a>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+            <div className='row'>
+                <div className='col-md-3 col-sm-6 col-xs-12 text-center'>
+                    <div className={styles.imgAlliance}>
+                        <Link href='https://www.instagram.com/vuelaelpez/'>
+                            <a>
+                                <Image src='/images/logovlpalianza.png' alt='Vuela el Pez' width='150' height='150' />
+                            </a>
+                        </Link>
+                    </div>
+                </div>
+
+                <div className='col-md-3 col-sm-6 col-xs-12 text-center'>
+                    <div className={styles.imgAlliance}>
+                        <Link href='https://www.instagram.com/movimientomeca/'>
+                            <a>
+                                <Image src='/images/logomecaalianza.png' alt='movimiento de espacios culturales y artísticos' width='150' height='150' />
+                            </a>
+                        </Link>
+                    </div>
+                </div>
+                <div className='col-md-3 col-sm-6 col-xs-12 text-center'>
+                    <div className={styles.imgAlliance}>
+                        <Link href='https://www.instagram.com/clicvillacrespo/'>
+                            <a>
+                                <Image src='/images/logoclicalianza.png' alt='Clic Villa Crespo' width='150' height='150' />
+                            </a>
+                        </Link>
+                    </div>
+                </div>
+                <div className='col-md-3 col-sm-6 col-xs-12 text-center'>
+                    <div className={styles.imgAlliance}>
+                        <Link href='https://www.instagram.com/casabrandon/'>
+                            <a>
+                                <Image src='/images/logocasabrandon.png' alt='Casa Brandon' width='150' height='150' />
                             </a>
                         </Link>
                     </div>
