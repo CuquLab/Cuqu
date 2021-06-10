cf5154197267e4b4b81a271629bfc6d4f900d056
Diana Gabriela Miños <eurekagaudeamus@gmail.com>
2021-06-10 12:31:36 -0300

    Renombro archivos y agrego estilos a botón

--- cuqulab/components/cardLeftImg.js
+++ cuqulab/components/cardLeftImg.js
@@ -1,10 +1,15 @@
 import Link from 'next/link'
+import styles from '../styles/Cards.module.css'
 
 
 const CardLeftImg = ({ title, img, text, href }) => {
     return (
         <div className='card shadow-sm mb-3' style={{ display: 'flex', width: '70%', height: '30vh', margin: 'auto', justifyContent: 'space-between', backgroundColor: '#F2F2F2' }}>
             <div className='row g-0' style={{ height: '100%' }}>
+                <div className='col-md-5' style={{
+                    backgroundImage: `url(${img})`, backgroundRepeat: 'no-repeat', backgroundPosition: 'center', backgroundSize: 'cover'
+                }} >
+                </div>
                 <div className='col-md-7 text-center'>
                     <div className='card-body mt-4'>
                         {text ?
@@ -12,16 +17,17 @@ const CardLeftImg = ({ title, img, text, href }) => {
                                 <h5 className='card-title'>{title}</h5>
                                 <p className='card-text'>{text}</p>
                                 <Link href={href}>
-                                    <button class='btn btn-outline-info' type='button'>+ info...</button>
+                                    <button className={styles.btnInfo} type='button'>+ info...</button>
                                 </Link>
                             </> :
-                            <h5 className='card-title' style={{ alignItems: 'center' }}>{title} </h5>
+                            <>
+                                <h5 className='card-title' style={{ alignItems: 'center' }}>{title} </h5>
+                                <Link href={href}>
+                                    <button className={styles.btnInfo} type='button'>+ info...</button>
+                                </Link>
+                            </>
                         }
                     </div>
-                </div>
-                <div className='col-md-5' style={{
-                    backgroundImage: `url(${img})`, backgroundRepeat: 'no-repeat', backgroundPosition: 'center', backgroundSize: 'cover'
-                }} >
                 </div>
             </div>
         </div>
--- cuqulab/pages/glosario.js
+++ cuqulab/pages/glosario.js
@@ -1,20 +1,18 @@
 
 import Container from '../components/container'
-import CardRightImg from '../components/cardRightImg'
 import CardLeftImg from '../components/cardLeftImg'
-
+import CardRightImg from '../components/cardRightImg'
 
 const Glosario = () => {
     return (
         <div>
             <Container>
-                <h2 className='text-center mb-5'>Glosario</h2>
+                <h2 className='text-center mb-5'>| Glosario |</h2>
                 <CardRightImg title='💪🏻 La unión hace la fuerza' img='/images/LaUnionHacelaFuerza.png' text='¡Acá encontrarás todas las redes culturales y colectivos de CABA para poder sumarte al que quieras!' href='https://www.notion.so/Glosario-2fb32dc90bb34909b486cbd55263a4a5' />
                 <CardLeftImg title='🏛 Organismos' img='/images/Organismos.png' text='En esta sección nombramos los organismos involucrados en el hacer cultural. ¡Es importante que sepas qué son y para qué sirven!' href='https://www.notion.so/Glosario-2fb32dc90bb34909b486cbd55263a4a5' />
                 <CardRightImg title='🚀 Espacios Culturales' img='/images/EspaciosCulturales.png' href='https://www.notion.so/Glosario-2fb32dc90bb34909b486cbd55263a4a5' />
                 <CardLeftImg title='🎖 Asociaciones Civiles' img='/images/AsociacionesCiviles.png' text='Acá repasamos qué información te piden habitualmente en los financiamientos y qué es cada una (en construcción)' href='https://www.notion.so/Glosario-2fb32dc90bb34909b486cbd55263a4a5' />
                 <CardRightImg title='🧰 Protocolos contra las violencias ' img='/images/ProtocolosContraLasViolencias.png' text='Acá encontrarás los protocolos contra las violencias de Géneros publicados (en construcción)' href='https://www.notion.so/Glosario-2fb32dc90bb34909b486cbd55263a4a5' />
-
             </Container>
         </div>
     )
