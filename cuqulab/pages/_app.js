f1090cfdeca6ad7abbed82b1c1a18d94aa27b3ba
Diana Gabriela Miños <eurekagaudeamus@gmail.com>
2021-06-12 20:54:46 -0300

    Modifico Footer dejando Home y Descargá nuestro manual como accesos rápidos, agrego link al Notion mediante ícono y dejo solo logo de CuquLab en primer columna

--- cuqulab/pages/_app.js
+++ cuqulab/pages/_app.js
@@ -11,7 +11,7 @@ function MyApp({ Component, pageProps }) {
         <link
           href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css' rel='stylesheet'
           integrity='sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1'
-          crossorigin='anonymous'
+          crossOrigin='anonymous'
         />
         <link
           rel="stylesheet"
@@ -20,7 +20,7 @@ function MyApp({ Component, pageProps }) {
         <script
           src='https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js'
           integrity='sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW'
-          crossorigin='anonymous' defer>
+          crossOrigin='anonymous' defer>
         </script>
       </Head>
       <Component {...pageProps} />
