136e6d5815bd1fc82144bafaf3f887b3cd2d50f5
Diana Gabriela Miños <eurekagaudeamus@gmail.com>
2021-06-14 01:22:28 -0300

    Creo nuevo diseño para componentes CardLeftImg y CarsRightImg para hacerlo responsive y para usarlo en componentes Glosario y Recursos y  agrego nuevos estilos

--- cuqulab/pages/glosario.js
+++ cuqulab/pages/glosario.js
@@ -11,8 +11,8 @@ const Glosario = () => {
                 <CardRightImg title='💪🏻 La unión hace la fuerza' img='/images/LaUnionHacelaFuerza.png' text='¡Acá encontrarás todas las redes culturales y colectivos de CABA para poder sumarte al que quieras!' href='https://www.notion.so/Glosario-2fb32dc90bb34909b486cbd55263a4a5' />
                 <CardLeftImg title='🏛 Organismos' img='/images/Organismos.png' text='En esta sección nombramos los organismos involucrados en el hacer cultural. ¡Es importante que sepas qué son y para qué sirven!' href='https://www.notion.so/Glosario-2fb32dc90bb34909b486cbd55263a4a5' />
                 <CardRightImg title='🚀 Espacios Culturales' img='/images/EspaciosCulturales.png' href='https://www.notion.so/Glosario-2fb32dc90bb34909b486cbd55263a4a5' />
-                <CardLeftImg title='🎖 Asociaciones Civiles' img='/images/AsociacionesCiviles.png' text='Acá repasamos qué información te piden habitualmente en los financiamientos y qué es cada una (en construcción)' href='https://www.notion.so/Glosario-2fb32dc90bb34909b486cbd55263a4a5' />
-                <CardRightImg title='🧰 Protocolos contra las violencias ' img='/images/ProtocolosContraLasViolencias.png' text='Acá encontrarás los protocolos contra las violencias de Géneros publicados (en construcción)' href='https://www.notion.so/Glosario-2fb32dc90bb34909b486cbd55263a4a5' />
+                <CardLeftImg title='🎖 Asociaciones Civiles' img='/images/AsociacionesCiviles.png' text='Acá repasamos qué información te piden habitualmente en los financiamientos y qué es cada una' href='https://www.notion.so/Glosario-2fb32dc90bb34909b486cbd55263a4a5' />
+                <CardRightImg title='🧰 Protocolos contra las violencias ' img='/images/ProtocolosContraLasViolencias.png' text='Acá encontrarás los protocolos contra las violencias de Géneros publicados' href='https://www.notion.so/Glosario-2fb32dc90bb34909b486cbd55263a4a5' />
             </Container>
         </div>
     )
