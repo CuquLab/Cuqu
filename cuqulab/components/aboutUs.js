48b2d68ee24dfc8a2791dc29641f5133c249d92c
Sofia Ferrer <chopotolina@gamil.com>
2021-06-24 17:03:41 -0300

    Descarga manual desde drive

--- cuqulab/components/aboutUs.js
+++ cuqulab/components/aboutUs.js
@@ -12,7 +12,7 @@ const About = () => {
                 <h5>Misión</h5>
                 <p>Cuqu promueve el derecho al acceso a una cultura diversa e inclusiva. Surge en el contexto de la pandemia del COVID19 en 2020 ante la falta de herramientas para la sostenibilidad de los proyectos culturales de la comunidad LGTBIQ+.</p>
                 <div className={styles.aboutLink}>
-                    <a target="blank" href="https://drive.google.com/drive/folders/1Pv_1DKlnK0ajV2p4YH6OtXk1Tcotz4vr">Descarga nuestro Manual</a>
+                    <a target="_blank" href="https://drive.google.com/drive/folders/1Pv_1DKlnK0ajV2p4YH6OtXk1Tcotz4vr">Descarga nuestro Manual</a>
                 </div>
             </div>
         </div>
